6aac91ae7b0c1bc6cea3ba82616abc2079797d6e
msorianol <marcsorianolluch@gmail.com>
2023-04-25 18:04:40 +0200

    animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,24 +4,35 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float Speed;
+    public float PlayerSpeed = 1.5f;
 
     public Rigidbody2D Rigidbody;
-    public Camera Camera;
+    private Camera Camera;
+    public Animator Animator;
 
     Vector2 movement;
+
     Vector2 mousePosition;
+
+    private void Start()
+    {
+        Animator = GetComponent<Animator>();
+    }
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-        mousePosition = Camera.ScreenToWorldPoint(Input.mousePosition);
-    }
+        Animator.SetFloat("Horizontal", movement.x);
+        Animator.SetFloat("Vertical", movement.y);
+        Animator.SetFloat("Speed", movement.sqrMagnitude);
 
+        //mousePosition = Camera.ScreenToWorldPoint(Input.mousePosition);
+    }
     private void FixedUpdate()
     {
-        Rigidbody.MovePosition(Rigidbody.position +  movement * Speed * Time.fixedDeltaTime);
+        Rigidbody.MovePosition(Rigidbody.position +  movement.normalized * PlayerSpeed * Time.fixedDeltaTime);
+        
 
         /*Vector2 direction = mousePosition - Rigidbody.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
