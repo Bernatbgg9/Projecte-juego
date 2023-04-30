6270001090a1993df5bd921937726e7fc3118ba5
msorianol <marcsorianolluch@gmail.com>
2023-04-30 21:17:21 +0200

    CameraImproved

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
-    public Transform player;
+    public Transform target;
 
-    public float smoothSpeed = 0.125f;
+    private Vector3 offset = new Vector3(0f, 0f, -1f);
+    public float smoothSpeed = 0.25f;
+    private Vector3 velocity = Vector3.zero;
+    public Vector3 minValues, maxValues;
 
-    void Update()
+    void FixedUpdate()
     {
-        transform.position = player.transform.position + new Vector3(0, 0, -1);
+        Vector3 targetPosition = target.position + offset;
+
+        Vector3 boundPosition = new Vector3(
+            Mathf.Clamp(targetPosition.x, minValues.x, maxValues.x),
+            Mathf.Clamp(targetPosition.y, minValues.y, maxValues.y),
+            Mathf.Clamp(targetPosition.z, minValues.z, maxValues.z));
+
+        //transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothSpeed);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, boundPosition, smoothSpeed * Time.fixedDeltaTime);
+        transform.position = smoothedPosition;
+        //transform.position = target.transform.position + new Vector3(0, 0, -1);
+
+        
     }
 }
