21f326e052272ff4ddc60c9888fb6c27aec158a5
msorianol <marcsorianolluch@gmail.com>
2023-06-08 12:04:08 +0200

    BetterScore

--- Assets/Scripts/Player/PlayerScore.cs
+++ Assets/Scripts/Player/PlayerScore.cs
@@ -5,17 +5,20 @@ using UnityEngine.UI;
 
 public class PlayerScore : MonoBehaviour
 {
-    public static int scoreValue = 0;
+    public static float scoreValue = 1000;
+    public float scoreDecreasePerSecond; 
 
     Text score;
 
     private void Start()
     {
         score = GetComponent<Text>();
+        scoreDecreasePerSecond = 1;
     }
 
     private void Update()
     {
-        score.text = "Score: " + scoreValue;
+        score.text = "Score: " + (int)scoreValue;
+        scoreValue -= scoreDecreasePerSecond * Time.deltaTime;
     }
 }
