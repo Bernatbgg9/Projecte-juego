e7488e24b1d3a8fa2a1c75ea7f4f6665f35bb579
msorianol <marcsorianolluch@gmail.com>
2023-06-09 15:15:22 +0200

    PickHeart

---
+++ Assets/Scripts/Loot/HandleLoot.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleLoot : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerHealth>().Heal(1);
+            Destroy(gameObject);
+        }
+    }
+}
